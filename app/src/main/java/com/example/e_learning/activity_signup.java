cd7282ba93346012cf44bf6fadaa59ad393c5158
quachthientai <83805773+quachthientai@users.noreply.github.com>
2022-03-09 02:13:16 -0600

    test test test

--- app/src/main/java/com/example/e_learning/activity_signup.java
+++ app/src/main/java/com/example/e_learning/activity_signup.java
@@ -10,5 +10,6 @@ public class activity_signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        //test test test
     }
 }
