5df7c0e7616488bfb1df0643fd182009cd0285b0
quachthientai <83805773+quachthientai@users.noreply.github.com>
2022-03-09 01:57:22 -0600

    test

---
+++ app/src/main/java/com/example/e_learning/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.e_learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Button signinBtnMain, signupBtnMain;
+    private ConstraintLayout parent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainViews();
+        signinBtnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"signinViews: Started");
+                Intent signIn = new Intent(MainActivity.this,activity_signin.class);
+                startActivity(signIn);
+            }
+        });
+
+        signupBtnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"signunViews: Started");
+                Intent signUp = new Intent(MainActivity.this,activity_signup.class);
+                startActivity(signUp);
+            }
+        });
+    }
+
+    //Initialize method, initialize all the button of home screen
+    private void mainViews() {
+        Log.d(TAG,"mainViews: Started");
+        signinBtnMain = findViewById(R.id.signinBtn);
+        signupBtnMain = findViewById(R.id.signupBtn);
+    }
+}
---
+++ app/src/main/java/com/example/e_learning/activity_signin.java
@@ -0,0 +1,117 @@
+package com.example.e_learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class activity_signin extends AppCompatActivity {
+
+    private static final String TAG = "SignInActivity";
+
+    private EditText editUserNameSI, editPasswordSI;
+    private TextView forgotPassSI, signinTextView, usernameWarnSI, passWarnSI;
+    private Button  loginBtnSI, signupBtnSI, backtoMain;
+    private ConstraintLayout childSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Sign in content view
+        setContentView(R.layout.activity_signin);
+
+        //Init firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        signinView();
+
+        backtoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "backtoMain: Started");
+                Intent backMain = new Intent(activity_signin.this,MainActivity.class);
+                startActivity(backMain);
+            }
+        });
+
+        signupBtnSI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUp = new Intent(activity_signin.this,activity_signup.class);
+                startActivity(signUp);
+            }
+        });
+
+        loginBtnSI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initLoginSI();
+            }
+        });
+
+        forgotPassSI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"forgotPassSI: Started");
+                Intent forgotPass = new Intent(activity_signin.this,activity_signup.class);
+                startActivity(forgotPass);
+            }
+        });
+    }
+
+    private void initLoginSI(){
+        Log.d(TAG, "loginViewSI: Started");
+        if (validateData()){
+
+        }
+    }
+
+    private boolean validateData(){
+        Log.d(TAG,"validateData: Started");
+        if ((editUserNameSI.getText().toString().equals("")) && (editPasswordSI.getText().toString().equals(""))){
+            usernameWarnSI.setVisibility(View.VISIBLE);
+            passWarnSI.setVisibility(View.VISIBLE);
+            return false;
+        }
+        else if (!(editUserNameSI.getText().toString().equals("")) && (editPasswordSI.getText().toString().equals(""))){
+            passWarnSI.setVisibility(View.VISIBLE);
+            return false;
+        }
+        else if ((editUserNameSI.getText().toString().equals("")) && !(editPasswordSI.getText().toString().equals(""))){
+            usernameWarnSI.setVisibility(View.VISIBLE);
+            return false;
+        }
+
+
+        return true;
+    }
+
+    private void signinView(){
+        Log.d(TAG,"signinView: Started");
+        editUserNameSI = findViewById(R.id.editUserName);
+        editPasswordSI = findViewById(R.id.editPassword);
+
+        forgotPassSI = findViewById(R.id.forgotPass);
+
+        backtoMain = findViewById(R.id.backtomain);
+        loginBtnSI = findViewById(R.id.loginBtn);
+        signupBtnSI = findViewById(R.id.signupBtn);
+
+        usernameWarnSI = findViewById(R.id.usernameWarn);
+        passWarnSI = findViewById(R.id.passWarn);
+        childSignIn = findViewById(R.id.childSignin);
+
+    }
+}
+
